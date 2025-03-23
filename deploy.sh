086d877f779164efb80bb5c0f2a870537791a284
TerezaG54 <Terezagarcia54@gmail.com>
2025-03-23 14:34:19 -0400

    Facts API

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-#/usr/bin/env sh
+#!/usr/bin/env sh
 set -e
 npm run build
 cd dist
 git init
 git add -A
-git commit -m 'New Deployment’
-git push -f git@github.com:USERNAME/vue-api-m10.git master:gh-pages
+git commit -m 'New Deployment'
+git push -f git@github.com:TerezaG54/vue-api-m10.git master:gh-pages
 cd –
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>TESTING </h1>
-  </div>
+	<div class="hello">
+		<h1>Random Fact</h1>
+		<button @click="fetchData">Click Me!</button>
+	</div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      fact: "",
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": 'your-api-key',
+          "X-RapidAPI-Host": 'facts-by-api-ninjas.p.rapidapi.com',
+        },
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            this.fact = data[0].fact;
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button {
+	padding: 12px 32px;
+	font-size: 16px;
+	border-radius: 8px;
 }
 </style>
